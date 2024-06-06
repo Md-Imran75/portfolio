137ad23352dddaf54b9dbcd5f122d7375f14f490
Md Imran Khan <ih912137@gmail.com>
2024-06-07 01:50:46 +0600

    first commit

---
+++ database/portfolio.js
@@ -0,0 +1,26 @@
+const protfolio = [
+    {
+        "projectName":"Ecommerce Website",
+        "code":"Html, Css, WordPress",
+        "liveUrl":"https://www.mactabeauty.com/",
+        "imageUrl":"../pic/3.PNG"
+    },
+    {
+        "projectName":"Ecommerce Website",
+        "code":"MERN Stack",
+        "liveUrl":"https://www.studokan.com",
+        "imageUrl":"../pic/1.PNG"
+    },
+    {
+        "projectName":"Business Website",
+        "code":"Html, Css, WordPress",
+        "liveUrl":"https://www.vemel.co.uk/",
+        "imageUrl":"../pic/Capture.PNG"
+    },
+    {
+        "projectName":"Business Website",
+        "code":"Html, Css, WordPress",
+        "liveUrl":"https://byfaux.se/",
+        "imageUrl":"../pic/545.PNG"
+    },
+]
